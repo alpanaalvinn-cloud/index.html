493da4f808b134fced343d7803d486af09754780
alpanaalvinn-cloud <alpanaalvinn@gmail.com>
2026-07-24 23:45:11 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// firebase.js
+
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkpSsKQ1oUkcMaqfz2Im9tVj8Yl9XkpYI",
+  authDomain: "alvin-website-cc21f.firebaseapp.com",
+  projectId: "alvin-website-cc21f",
+  storageBucket: "alvin-website-cc21f.firebasestorage.app",
+  messagingSenderId: "1031795933286",
+  appId: "1:1031795933286:web:7bff285fc6589271f76da7",
+  measurementId: "G-26ZLF4L1RK"
+};
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
