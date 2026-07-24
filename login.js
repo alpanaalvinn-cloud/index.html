4629b5db53d1c24d6ea2b1167770c982f15daa62
alpanaalvinn-cloud <alpanaalvinn@gmail.com>
2026-07-25 01:05:16 +0700

    Update login.js

--- login.js
+++ login.js
@@ -10,34 +10,25 @@ const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", () => {
 
-
 const email = document.getElementById("email").value.trim();
 const password = document.getElementById("password").value.trim();
 
-console.log("Email:", email);
-console.log("Password:", password);
+console.log(email);
 
-if(!email || !password){
-    alert("Email dan password wajib diisi");
+if(email === "" || password === ""){
+    alert("Isi email dan password dulu");
     return;
 }
 
 signInWithEmailAndPassword(auth,email,password)
 
 .then(() => {
-
-alert("Login berhasil!");
-
-window.location.href="index.html";
-
+    alert("Login berhasil!");
+    window.location.href="index.html";
 })
 
-
 .catch((error)=>{
-
-alert("Login gagal: " + error.message);
-
+    alert("Login gagal: " + error.message);
 });
-
 
 });
